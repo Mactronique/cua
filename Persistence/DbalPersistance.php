599bf08e1a20bf83a3d70093bca53b92bb4265f9
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2016-02-13 17:06:56 +0100

    dbal

--- Persistence/DbalPersistance.php
+++ Persistence/DbalPersistance.php
@@ -9,41 +9,97 @@
  */
 namespace Mactronique\CUA\Persistence;
 
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
+class DbalPersistance implements Persistence
+{
+    /**
+     * @var string default plath of file
+     */
+    private $config;
 
+    private $connexion;
 
-class DbalPersistance implements Persistence{
-	/**
-	 * @var string default plath of file
-	 */
-	private $config;
+    /**
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
 
-	private $connexion;
+    /**
+     * @param array $content Content to save
+     * @param array $config  custom config
+     */
+    public function save(array $content, array $config = null)
+    {
+        $this->connexion = new \Doctrine\Dbal\Connexion($this->config);
 
-	/**
-	 * @param array $config
-	 */
-	public function __construct(array $config){
-		$this->config = $config;
-	}
+    }
 
-	/**
-	 * @param array $content Content to save
-	 * @param array $config custom config
-	 */
-	public function save(array $content, array $config = null)
-	{
-		$this->connexion = new \Doctrine\Dbal\Connexion($this->config);
+    /**
+     * Manage the installed library
+     * @param string $projet
+     * @param array  $installed library list
+     */
+    private function installedLib($project, array $installed)
+    {
+    }
 
-		$connexion
-	}
+    /**
+     * Manage the library to install
+     * @param string $projet
+     * @param array  $install library list
+     */
+    private function installLib($project, array $install)
+    {
+    }
 
+    /**
+     * Manage the library to update
+     * @param string $projet
+     * @param array  $update library list
+     */
+    private function updateLib($project, array $update)
+    {
+    }
 
-	private function checkExist($project, $library){
-		$result = $this->connexion->execute('SELECT count(*) as nombre FROM '.$config['table_name']. ' WHERE project= ? AND library=?', [$project, $library]);
-		$nb = $result->fetch();
+    /**
+     * Manage the library to remove
+     * @param string $projet
+     * @param array  $remove library list
+     */
+    private function removeLib($project, array $remove)
+    {
+    }
 
-		return $nb['nombre']!=0;
-	}
+    /**
+     * Manage the abandoned library
+     * @param string $projet
+     * @param array  $abandonned library list
+     */
+    private function abandonedLib($project, array $abandonned)
+    {
+    }
+
+    private function checkExist($project, $library)
+    {
+        $result = $this->connexion->execute('SELECT count(*) as nombre FROM '.$config['table_name']. ' WHERE project= ? AND library=?', [$project, $library]);
+        $nb = $result->fetch();
+
+        return $nb['nombre']!=0;
+    }
+
+    /**
+     * @param array $data list of field (key) with value.
+     */
+    private function insert(array $data)
+    {
+    }
+
+    /**
+     * @param array $data list of field (key) with value.
+     */
+    private function update(array $data)
+    {
+    }
 }
