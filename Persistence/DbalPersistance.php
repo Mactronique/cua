bfa8bc78ebfb074d340c197ba56a80abb0701f87
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2016-02-13 15:24:27 +0100

    add persistance dbal

--- Persistence/DbalPersistance.php
+++ Persistence/DbalPersistance.php
@@ -11,10 +11,6 @@ namespace Mactronique\CUA\Persistence;
 
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Exception\ProcessTimedOutException;
-use Symfony\Component\Yaml\Yaml;
 
 
 class DbalPersistance implements Persistence{
@@ -22,6 +18,8 @@ class DbalPersistance implements Persistence{
 	 * @var string default plath of file
 	 */
 	private $config;
+
+	private $connexion;
 
 	/**
 	 * @param array $config
@@ -36,6 +34,16 @@ class DbalPersistance implements Persistence{
 	 */
 	public function save(array $content, array $config = null)
 	{
-		
+		$this->connexion = new \Doctrine\Dbal\Connexion($this->config);
+
+		$connexion
+	}
+
+
+	private function checkExist($project, $library){
+		$result = $this->connexion->execute('SELECT count(*) as nombre FROM '.$config['table_name']. ' WHERE project= ? AND library=?', [$project, $library]);
+		$nb = $result->fetch();
+
+		return $nb['nombre']!=0;
 	}
 }
