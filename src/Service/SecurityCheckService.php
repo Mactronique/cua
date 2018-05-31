1b08598a46e97f47866d4bab147999947b282d7f
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2018-05-31 23:46:06 +0200

    include security checker

--- src/Service/SecurityCheckService.php
+++ src/Service/SecurityCheckService.php
@@ -11,9 +11,7 @@ namespace InExtenso\CUA\Service;
 
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Exception\ProcessTimedOutException;
+use SensioLabs\Security\SecurityChecker;
 
 class SecurityCheckService
 {
@@ -35,32 +33,14 @@ class SecurityCheckService
             'result' => [],
         ];
 
-        $process = new Process($php_path.' '.$this->securityCheckPath.' security:check '.$lockPath.' --format=json ');
-        $process->setWorkingDirectory($projectPath);
-        $process->setTimeout(3000);
+        $checker = new SecurityChecker();
         try {
-            $returnCode = $process->run();
-        } catch (ProcessTimedOutException $e) {
-            $this->logger->error('Process time out', ['projectPath'=>$projectPath, 'exception'=>$e]);
-            $resultProject['error'] = 'Time out '.$e->getMessage();
-            return $resultProject;
-        }
+            $datas = $checker->check($lockPath);
 
-        //var_dump($returnCode);
-        $sortieErr = $process->getErrorOutput();
-        if (strlen($sortieErr) > 0) {
-            $resultProject['error'] = $sortieErr;
+            $resultProject['result'] = $datas;
+        } catch(\Exception $e) {
+            $resultProject['error'] = $e->getMessage();
         }
-
-        $sortie = $process->getOutput();
-        $datas = json_decode($sortie, true);
-
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            $this->logger->error('Json decode error', ['projectPath'=>$projectPath, 'json_error'=>json_last_error_msg()]);
-            $resultProject['error'] .= 'Json decode error '.json_last_error_msg();
-            return $resultProject;
-        }
-        $resultProject['result'] = $datas;
 
         return $resultProject;
     }
